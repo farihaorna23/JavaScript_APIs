0d6d983371acf445da1e4b62811025328925c76a
farihaorna23 <farihaorna@gmail.com>
2022-09-05 23:50:13 -0400

    Finished Exercise 2 and 3

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+
+const APP_KEY = "XgUi9apSviffD2ENh44TPQYg34e2o8yB";
+const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate";
+
 let searchBtn = document.getElementById("submitSearch");
 let searchInput = document.getElementById("searchWord");
 let image = document.getElementById("imageContainer");
 let paragraph = document.querySelector("p");
 
-console.log(searchBtn, searchInput, image, paragraph);
+//console.log(searchBtn, searchInput, image, paragraph);
+
+searchBtn.addEventListener("click", () => {
+  let gifValue = searchInput.value;
+  fetch(`${GIPHY_URL}?api_key=${APP_KEY}&s=${gifValue}`)
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => console.error(error));
+});
