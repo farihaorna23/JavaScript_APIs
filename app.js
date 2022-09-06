46946ec242eaae14efb3cec33ac6fe6f5dcf12f2
farihaorna23 <farihaorna@gmail.com>
2022-09-05 22:58:47 -0400

    Finished Exercise1

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+let searchBtn = document.getElementById("submitSearch");
+let searchInput = document.getElementById("searchWord");
+let image = document.getElementById("imageContainer");
+let paragraph = document.querySelector("p");
+
+console.log(searchBtn, searchInput, image, paragraph);
